a4ba0f638aa67c6df6a5c39ad02b96302c258fa3
nsing <nsing@users.noreply.github.com>
2014-04-16 22:55:16 +0100

    Update FindTopValuesImpl.java
    work in progress

--- src/main/java/com/exercise/FindTopValuesImpl.java
+++ src/main/java/com/exercise/FindTopValuesImpl.java
@@ -1,40 +1,49 @@
-package com.exercise;
+package test;
 
 /**
- * This class uses partial sorting algorithm based on Quicksort
- * Expected time is O(s + n log n) where s is the size of array
- * and n is the no. top elements.
+ * Implements interface {@link FindTopValues}
  */
 public class FindTopValuesImpl implements FindTopValues {
 
 	/**
-	 * This method returns the single highest integer in the array.
+	 * Returns the single highest integer in the array.
+	 * 
+	 * @param input array
+	 * @return max value
 	 */
 	public int findMaxValue(int[] anyOldOrderValues) {
 		return findTopNValues(anyOldOrderValues, 1)[0];
 	}
 
 	/**
-	 * This method returns an array of the highest ’n’ values in the source array,
+	 * Returns an array of the highest n values in the source array,
 	 * ordered naturally, with the highest value at the start of the array.
+	 * This uses partial sorting algorithm based on Quicksort. Expected 
+	 * time is O(s + n log n) where s is the size of array and n is the no.
+	 * of top elements.
+	 * 
+	 * @param input array
+	 * @param no. of top values
+	 * @return array of top values
 	 */
+ 
 	public int[] findTopNValues(int[] anyOldOrderValues, int n) {
 		int[] topValues = new int[n];
 		quickfindFirstK(anyOldOrderValues, 0, anyOldOrderValues.length-1, n);
 		for(int i=0; i< n; i++) {
 			topValues[i] = anyOldOrderValues[i];
 		}
-	    return topValues;
+		return topValues;
 	}
 
 	/**
-	 * This function is Quicksort variation.
-	 * If the pivot falls in position k or later, recurse only on the left partition
+	 * If the pivot falls in position n or later, recurse only on the left
+	 * partition
 	 *
 	 * @param array
 	 * @param left
 	 * @param right
-	 * @param n
+	 * @param no. of top values
 	 */
 	private void quickfindFirstK(int[] array, int left, int right, int n) {
 		if (right > left) {
@@ -52,38 +61,38 @@ public class FindTopValuesImpl implements FindTopValues {
 	 * by moving all elements equal or greater than array[pivotIndex] before the pivot,
 	 * and the smaller elements after it.
 	 *
-	 * @param anyOldOrderValues
+	 * @param array
 	 * @param left
 	 * @param right
 	 * @param pivotIndex
 	 * @return
 	 */
-	private int partition(int[] anyOldOrderValues, int left, int right, int pivotIndex) {
-		int pivotValue = anyOldOrderValues[pivotIndex];
-		swap(anyOldOrderValues, pivotIndex, right);
+	private int partition(int[] array, int left, int right, int pivotIndex) {
+		int pivotValue = array[pivotIndex];
+		swap(array, pivotIndex, right);
 		int storeIndex = left;
-	    for (int i=left; i < right; i++) {
-	        if (anyOldOrderValues[i] > pivotValue) {
-	        	swap(anyOldOrderValues, i, storeIndex);
-	            storeIndex++;
-	        }
-	    }
-	    swap(anyOldOrderValues, storeIndex, right);
-	    return storeIndex;
+		for (int i=left; i < right; i++) {
+			if (array[i] > pivotValue) {
+				swap(array, i, storeIndex);
+			    	storeIndex++;
+			}
+		}
+		swap(array, storeIndex, right);
+		return storeIndex;
 	}
 
 	/**
 	 * Swap given indices values in an array
 	 *
-	 * @param anyOldOrderValues
-	 * @param index1
-	 * @param index2
+	 * @param array
+	 * @param index 1
+	 * @param index 2
 	 */
-    private void swap(int[] anyOldOrderValues, int index1, int index2) {
-        int tmp = anyOldOrderValues[index1];
-        anyOldOrderValues[index1] = anyOldOrderValues[index2];
-        anyOldOrderValues[index2] = tmp;
-    }
+	private void swap(int[] array, int i1, int i2) {
+		array[i1] = array[i1] + array[i2];
+		array[i2] = array[i1] - array[i2]
+		array[i1] = array[i1] - array[i2];
+	}
 
 	/**
 	 * Main method
@@ -93,10 +102,10 @@ public class FindTopValuesImpl implements FindTopValues {
 	public static void main(String[] args) {
 		FindTopValues findTopValues = new FindTopValuesImpl();
 		int[] arr = { 1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-				     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-				     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-				     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-				     41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+			      11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+			      21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			      31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+			      41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
 		System.out.println("Array is:: ");
 		for(int i=0; i<arr.length; i++){
 			System.out.print(arr[i]+" ");
