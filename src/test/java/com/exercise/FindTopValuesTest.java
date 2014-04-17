7a5b360d2e49b83efca792a0c7e893b745ea048d
nsing <nsing@users.noreply.github.com>
2014-04-17 09:25:49 +0100

    Update FindTopValuesTest.java

--- src/test/java/com/exercise/FindTopValuesTest.java
+++ src/test/java/com/exercise/FindTopValuesTest.java
@@ -1,4 +1,4 @@
-package com.exercise;
+package test;
 
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -21,9 +21,9 @@ FindTopValues findTopValues;
 	public void findTopValuesTest() {
 		int[] arr = { 1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
 		              11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-					  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-					  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-					  41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+			      21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			      31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+			      41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
 		int[] topValuesActual = findTopValues.findTopNValues(arr, 5);
 		int[] topValuesExpected = {50, 49, 48, 47, 46};
 		assertArrayEquals(topValuesExpected, topValuesActual);
@@ -33,9 +33,9 @@ FindTopValues findTopValues;
 	public void findMaxValueTest() {
 		int[] arr = { 1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
 		              11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-					  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-					  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-					  41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+			      21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			      31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+			      41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
 		int maxValueActual = findTopValues.findMaxValue(arr);
 		int maxValueExpected = 50;
 		assertEquals(maxValueExpected, maxValueActual);
